65e9398d05a3c10d132ba0fd8e8cd80c40597219
𝒅𝒓𝒆𝒂𝒎𝒔 <mybeautifuladress@gmail.com>
2025-10-29 16:18:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,8 @@ how many vertical characters : 8
 # librairies
 from machine import Pin, I2C
 from picobricks import SSD1306_I2C
-import time, random
+import time
+import random
 
 # pins
 i2c = I2C(0, scl=Pin(5), sda=Pin(4))
@@ -68,44 +69,53 @@ CHAR_WIDTH = 8
 CHAR_HEIGHT = 8
 COLS = WIDTH // CHAR_WIDTH
 # may influence fps and drops's spe
-UPDATE_DELAY = 0.05
+UPDATE_DELAY = 0.005
 
 # we'll pick random value between these int to determinate current drop lenght
 DROPS_MIN_LENGHT = 1
-DROPS_MAX_LENGHT = 5
+DROPS_MAX_LENGHT = 6
 
-# drops is a list that contains every vertical positions of every droplets
-# vertical position can vary from 0 to 63 (since height is 64)
-drops = [random.randint(0, HEIGHT - 1) for _ in range(COLS)] 
+drops = [random.randint(-(int(HEIGHT/2)), 0) for _ in range(COLS)] 
+current_tail_lenght = [random.randint(DROPS_MIN_LENGHT + 1, DROPS_MAX_LENGHT) for _ in range(COLS)]
 
 # head and tail have different character sets
 head_chars = ["*"]
 tail_chars = list("abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*(){}[];:/?\|+<>")
 
+
 while True:
     # clear the screen
     oled.fill(0)
+    #shuffle(drops_preset)
 
-    for i in range(COLS):
+    
+    for i in range(COLS / 2):
         # get horizontal position of cols, depends on CHAR_WIDTH
-        x = i * CHAR_WIDTH
+        x = i * CHAR_WIDTH * 2
         # get vertical position of current drop stored in drops[]
         y = drops[i] * CHAR_HEIGHT
         
         # show head
         oled.text(random.choice(head_chars), x, y)
+        
+        for a in range(DROPS_MIN_LENGHT, current_tail_lenght[i]):
+            tail_y = y - a * CHAR_HEIGHT
+            oled.text(random.choice(tail_chars), x, tail_y)
 
+            
         # increase current drop's lenght
         drops[i] += 1
 
         # if current drop touches edge of the screen
         if drops[i] * CHAR_HEIGHT > (HEIGHT + DROPS_MAX_LENGHT * CHAR_HEIGHT):
             # reset current drop, multiple of CHAR_HEIGHT
-            drops[i] = n = random.randint(0, 4) * CHAR_HEIGHT
+            drops[i] = random.randint(-int(HEIGHT/8), 0) * CHAR_HEIGHT
+            current_tail_lenght[i] = random.randint(DROPS_MIN_LENGHT + 1, DROPS_MAX_LENGHT)
 
 
     # finally show result by updating screen
     oled.show()
     time.sleep(UPDATE_DELAY)
+
 
 
